179e096c61775503396850981fd7f32dfc18e8d8
rickastlee <45075899+rickastlee@users.noreply.github.com>
2025-03-23 01:04:22 +0100

    sm6250-common: Switch to Gemini boot animation

--- atoll.mk
+++ atoll.mk
@@ -86,6 +86,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Boot animation
+TARGET_BOOT_ANIMATION_GEMINI := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
